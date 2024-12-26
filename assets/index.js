b2c06a1ba9e9c985245d6427e9a563e1b5220e50
Hariom ATPL <applatus.hariom@gmail.com>
2024-12-26 23:22:01 +0530

    initial commit

---
+++ assets/index.js
@@ -0,0 +1,12 @@
+function toggleNav() {
+    const navLinks = document.getElementById("nav-links");
+    navLinks.classList.toggle("active"); // Toggle the 'active' class to show/hide nav links
+}
+
+var typed = new Typed('#element', {
+    strings: ['Web Designer', 'Graphics Designer', 'Video Editor', 'Web Developer'],
+    typeSpeed: 30,
+    backSpeed: 20,
+    loop: true,
+    startDelay: 1000
+}); 
